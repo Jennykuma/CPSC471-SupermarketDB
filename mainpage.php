5498d128e2f5db1982a541339e49dcea05ebd701
Jenny <sacxjenny@live.com>
2017-04-01 21:05:20 -0600

    Needa make some changes..

--- mainpage.php
+++ mainpage.php
@@ -45,46 +45,8 @@ Supermarket Database
             </div>
         </div>
 
+        <?php
+        ?>
 
-        <div class="phpStuff">
-            <?php
-                // put your code here
-                $servername = "localhost";          //should be same for you
-                $username = "root";                 //same here
-                $password = "rootPass";             //your localhost root password
-                $db = "supermarket";                     //your database name
-
-                $conn = new mysqli($servername, $username, $password, $db);
-
-                if($conn->connect_error){
-                    die("Connection failed".$conn->connect_error);
-                }else{
-                    echo "Connected<br>";
-                }
-
-                //sql query
-                $sql = "INSERT INTO names (names) VALUES ('John')";
-                echo "<br><br>Inserting  into db: ";
-                if($conn->query($sql)==TRUE){       //try executing the query
-                    echo "Query executed<br>";
-                }
-                else{
-                    echo "Query did not execute<br>";
-                }
-
-                //sql query
-                $sql = "SELECT names FROM names";
-                echo "<br><br>Printing names in the (names) table in the (names) column:<br>";
-                $result = $conn->query($sql);       //execute the query
-
-                if($result->num_rows >0){           //check if query results in more than 0 rows
-                    while($row = $result->fetch_assoc()){   //loop until all rows in result are fetched
-                        echo "Name: ".$row["names"]."<br>"; //here we are looking at one row, and printing the value in "names" column
-                    }
-                }
-
-                $conn-> close();            //close the connection to database
-            ?>
-        </div>
     </body>
 </html>
