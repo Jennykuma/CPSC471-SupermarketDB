6db9516ae8b6cb64e028ccb5a9ae3266dd701d68
auralapse <auralapse@gmail.com>
2017-04-04 11:40:26 -0600

    Made file for customer, jsut starting

---
+++ customerFront.php
@@ -0,0 +1,29 @@
+<!--Customer Page: includes mainMenu, productList, givesFeedback, -->
+
+
+
+
+<?php
+
+/*
+TODO:
+
+Main Menu:
+- Welcome <customer name> 
+
+Product List:
+- include drop down list from "Department"
+- List products by Department
+
+Give Feedback:
+- Rating selection /10 
+- Text input for feedback 
+- submit button 
+*/
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
