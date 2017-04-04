1b500a892c533666fc7f78b24dda3bbee8ab3106
archan <archan@ucalgary.ca>
2017-04-03 19:23:14 -0600

    added foreign keys and some entries

--- supermarket_db.sql
+++ supermarket_db.sql
@@ -1,6 +1,6 @@
 ﻿# Host: localhost  (Version 5.7.17-log)
-# Date: 2017-04-01 22:34:55
-# Generator: MySQL-Front 6.0  (Build 1.74)
+# Date: 2017-04-03 19:22:00
+# Generator: MySQL-Front 6.0  (Build 1.78)
 
 
 #
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS `customer`;
 CREATE TABLE `customer` (
   `cid` int(8) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `phone_num` int(10) DEFAULT NULL,
+  `phone_num` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`cid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -19,6 +19,7 @@ CREATE TABLE `customer` (
 # Data for table "customer"
 #
 
+INSERT INTO `customer` VALUES (1,'Saul Goodman','5055034455'),(2,'Jane Underwood','7891457497');
 
 #
 # Structure for table "department"
@@ -35,24 +36,7 @@ CREATE TABLE `department` (
 # Data for table "department"
 #
 
-INSERT INTO `department` VALUES ('Bakery',1);
-
-#
-# Structure for table "dependent"
-#
-
-DROP TABLE IF EXISTS `dependent`;
-CREATE TABLE `dependent` (
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `emp_id` int(8) NOT NULL,
-  `relation` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`name`,`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "dependent"
-#
-
+INSERT INTO `department` VALUES ('Bakery',1),('Seafood',2),('Management',3),('Meat',4);
 
 #
 # Structure for table "employee"
@@ -69,14 +53,38 @@ CREATE TABLE `employee` (
   `phone_num` varchar(12) DEFAULT NULL,
   `dep_name` varchar(255) DEFAULT NULL,
   `super_id` int(8) DEFAULT NULL,
-  PRIMARY KEY (`eid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`eid`),
+  KEY `emp_id_fk3` (`super_id`),
+  KEY `dep_name_fk3` (`dep_name`),
+  CONSTRAINT `dep_name_fk3` FOREIGN KEY (`dep_name`) REFERENCES `department` (`dname`),
+  CONSTRAINT `emp_id_fk3` FOREIGN KEY (`super_id`) REFERENCES `employee` (`eid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "employee"
 #
 
-INSERT INTO `employee` VALUES (1,'Jenny','Le',123456789,'Boss',9000,'1234567890','Manager',NULL),(2,'Aron','Chan',987654321,'Boss',10000,'9876543210','Manager',NULL);
+INSERT INTO `employee` VALUES (1,'Jenny','Le',123456789,'Boss',9000,'1234567890','Management',NULL),(2,'Aron','Chan',987654321,'Boss',10000,'9876543210','Management',NULL),(3,'Mary','Wang',459721324,'Cashier',10,'8462485555','Management',1),(4,'Martin','Wong',444975346,'Cashier',10,'4894511572','Management',2),(5,'Gordon','Lee',775245619,'Butcher',10,'4897526548','Meat',2),(6,'Henry','Wilson',159724896,'Baker',10,'7663548941','Bakery',1),(7,'Sarah','Jones',784115745,'Sushi Chef',10,'4897941859','Seafood',1);
+
+#
+# Structure for table "dependent"
+#
+
+DROP TABLE IF EXISTS `dependent`;
+CREATE TABLE `dependent` (
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `emp_id` int(8) NOT NULL,
+  `relation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`name`,`emp_id`),
+  KEY `emp_id_fk` (`emp_id`),
+  CONSTRAINT `emp_id_fk` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`eid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "dependent"
+#
+
+INSERT INTO `dependent` VALUES ('Carol Lee',5,'Spouse'),('Joey Wong',4,'Father'),('Marie Wong',4,'Mother');
 
 #
 # Structure for table "gives_feedback"
@@ -98,6 +106,7 @@ CREATE TABLE `gives_feedback` (
 # Data for table "gives_feedback"
 #
 
+INSERT INTO `gives_feedback` VALUES (1,'Management',5,NULL),(2,'Management',2,'They wouldn\'t accept my coupon!');
 
 #
 # Structure for table "names"
@@ -117,6 +126,49 @@ CREATE TABLE `names` (
 INSERT INTO `names` VALUES (1,'John'),(2,'John'),(3,'John'),(4,'John'),(5,'John'),(6,'John');
 
 #
+# Structure for table "shift"
+#
+
+DROP TABLE IF EXISTS `shift`;
+CREATE TABLE `shift` (
+  `shift_num` int(8) NOT NULL,
+  `date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `emp_id` int(11) DEFAULT NULL,
+  `dep_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`shift_num`),
+  KEY `dep_name_fk2` (`dep_name`),
+  KEY `emp_id_fk2` (`emp_id`),
+  CONSTRAINT `dep_name_fk2` FOREIGN KEY (`dep_name`) REFERENCES `department` (`dname`),
+  CONSTRAINT `emp_id_fk2` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`eid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "shift"
+#
+
+INSERT INTO `shift` VALUES (1,'2017-04-16','09:00:00','17:00:00',3,'Management'),(2,'2017-04-16','09:00:00','17:00:00',4,'Management'),(3,'2017-04-16','08:00:00','17:00:00',5,'Meat'),(4,'2017-04-16','08:00:00','17:00:00',6,'Bakery'),(5,'2017-04-16','08:00:00','17:00:00',7,'Seafood');
+
+#
+# Structure for table "supplier"
+#
+
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE `supplier` (
+  `name` varchar(255) DEFAULT NULL,
+  `phone_num` varchar(12) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "supplier"
+#
+
+INSERT INTO `supplier` VALUES ('SupplyMarket','4038741569','1220 Breeze St. NW'),('Snack Stocker Inc.','7489415799','12 742 Ave. SE');
+
+#
 # Structure for table "product"
 #
 
@@ -127,14 +179,16 @@ CREATE TABLE `product` (
   `price` decimal(5,2) DEFAULT NULL,
   `sup_name` varchar(255) DEFAULT NULL,
   `wholesale_price` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`pid`),
+  KEY `sup_name_fk` (`sup_name`),
+  CONSTRAINT `sup_name_fk` FOREIGN KEY (`sup_name`) REFERENCES `supplier` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "product"
 #
 
-INSERT INTO `product` VALUES (1,'Bread',NULL,NULL,NULL);
+INSERT INTO `product` VALUES (1,'Bread',5.99,'SupplyMarket',3.99),(2,'Instant Noodles (12 Pk)',11.99,'Snack Stocker Inc.',9.99);
 
 #
 # Structure for table "sells"
@@ -155,44 +209,7 @@ CREATE TABLE `sells` (
 # Data for table "sells"
 #
 
-INSERT INTO `sells` VALUES ('Bakery',1);
-
-#
-# Structure for table "shift"
-#
-
-DROP TABLE IF EXISTS `shift`;
-CREATE TABLE `shift` (
-  `shift_num` int(8) NOT NULL,
-  `date` date DEFAULT NULL,
-  `start_time` time DEFAULT NULL,
-  `end_time` time DEFAULT NULL,
-  `emp_id` int(11) DEFAULT NULL,
-  `dep_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`shift_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "shift"
-#
-
-
-#
-# Structure for table "supplier"
-#
-
-DROP TABLE IF EXISTS `supplier`;
-CREATE TABLE `supplier` (
-  `name` varchar(11) DEFAULT NULL,
-  `phone_num` varchar(12) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "supplier"
-#
-
+INSERT INTO `sells` VALUES ('Bakery',1),('Management',2);
 
 #
 # Structure for table "transaction"
@@ -205,12 +222,13 @@ CREATE TABLE `transaction` (
   `price` decimal(5,2) DEFAULT NULL,
   `payment_method` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "transaction"
 #
 
+INSERT INTO `transaction` VALUES (1,'2017-03-15 09:35:00',18.99,'Debit'),(2,'2017-03-15 10:20:00',37.98,'Cash');
 
 #
 # Structure for table "makes"
@@ -225,9 +243,10 @@ CREATE TABLE `makes` (
   KEY `trans_id_fk` (`trans_id`),
   CONSTRAINT `cust_id_fk` FOREIGN KEY (`cust_id`) REFERENCES `customer` (`cid`),
   CONSTRAINT `trans_id_fk` FOREIGN KEY (`trans_id`) REFERENCES `transaction` (`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "makes"
 #
 
+INSERT INTO `makes` VALUES (1,2),(2,1);
